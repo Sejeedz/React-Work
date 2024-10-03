e2e04357c44cfff4cb73d8627a2dced212de6f56
Sejeedz <m.sajid3421@gmail.com>
2024-10-04 02:04:46 +0500

    React Quiz refactored using Context API!

--- react-quiz/src-no-context/components/DateCounter.js
+++ react-quiz/src-no-context/components/DateCounter.js
@@ -2,7 +2,10 @@ import { useReducer } from "react";
 const initialState = { count: 0, step: 1 };
 function reducer(state, action) {
   console.log(state, action);
-
+  // if (action.type === "inc") return state + 1;
+  // if (action.type === "dec") return state - 1;
+  // if (action.type === "setCount") return action.payload;
+  // return { count: 0, step: 1 };
   switch (action.type) {
     case "dec":
       return { ...state, count: state.count - state.step };
